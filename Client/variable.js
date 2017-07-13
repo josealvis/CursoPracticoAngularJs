ba16f0168cc5a0c432c60804c49793c30118960f
jose alvis <jalvis1491@gmail.com>
2017-07-12 20:32:44 -0400

    Agrega el archivo de la practiva de las variables.

---
+++ Client/variable.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict'
+
+var miVariable, otraVariable, yOtraVariable;
+
+
+miVariable =  "hola mundo!!";
+
+console.log(typeof(miVariable));
+
+
+console.log(miVariable);
+
+})();
