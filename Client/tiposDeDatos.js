a943d82e5ec9f1f5da3ed5c3c8aba4322d060d52
jose alvis <jalvis1491@gmail.com>
2017-07-12 20:43:39 -0400

    Agrega el Archivo tipo de datos Tipos de datos en javascript.

---
+++ Client/tiposDeDatos.js
@@ -0,0 +1,18 @@
+(function() {
+   'use strict' 
+
+
+// tipos primitivos
+var miVariable; //undefined
+    miVariable = 4;// number
+    miVariable = "Un texto";// string
+    miVariable = true; // boleano
+    miVariable = null; //object
+
+    // tipos complejos
+
+    miVariable = [1,"texto", true] // objeto
+    miVariable = {nombre: "José", edad:26, coloresFavoritos: ["azul", "Rojo"] }// objet
+    miVariable = function (){};// function
+
+})();
