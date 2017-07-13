db04b1da118ff2b83524c80623a3abf5c0006477
jose alvis <jalvis1491@gmail.com>
2017-07-12 20:44:22 -0400

    Agrega output Agrega output para los tipos de datos.

--- Client/tiposDeDatos.js
+++ Client/tiposDeDatos.js
@@ -4,15 +4,23 @@
 
 // tipos primitivos
 var miVariable; //undefined
+    console.log("tipo Primitivo: ", typeof(miVariable));
     miVariable = 4;// number
+    console.log("tipo Primitivo: ", typeof(miVariable));
     miVariable = "Un texto";// string
+    console.log("tipo Primitivo: ", typeof(miVariable));
     miVariable = true; // boleano
+    console.log("tipo Primitivo: ", typeof(miVariable));
     miVariable = null; //object
+    console.log("tipo Primitivo: ", typeof(miVariable));
 
     // tipos complejos
 
     miVariable = [1,"texto", true] // objeto
+    console.log("tipo complejo: ", typeof(miVariable));
     miVariable = {nombre: "José", edad:26, coloresFavoritos: ["azul", "Rojo"] }// objet
+    console.log("tipo complejo: ", typeof(miVariable));
     miVariable = function (){};// function
+    console.log("tipo complejo: ", typeof(miVariable));
 
 })();
