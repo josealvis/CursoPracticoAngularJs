ae4f83487898fd79340c8413b238a268d9d8dc27
Jose Alvis Peña Bonnet <jbonnet@arssenasa.local>
2017-07-17 09:41:51 -0400

    Operadores

---
+++ Client/operadores.js
@@ -0,0 +1,47 @@
+(function(){
+'use strict'
+
+
+// operadores arictmeticos
+   var suma = 5 +5;
+   var resta =  5-2;
+   var division = 12/3;
+   var multiplicacion = 4*4;
+   var modulo = 12%2;
+       modulo = 7%2;
+       suma++;
+       resta--; 
+   var ordenGerarquico = 2*5+3*6;
+
+
+ // operadores de asignación
+  var x = 1;
+      x += 2;// x= x+2;
+      x -= 1;//x= x-1;
+      x *=2;//x = x*2;
+      x /=2;// x= x/2;
+      x %=2; //x = x%2;
+      
+
+ //operadores con strings
+    var str1 = "Jose";
+    var str2 = " Alvis"
+    var str3 =  str1 +str2;
+    var str4 =  2 + " libras"; 
+    var str5 = 2 + 4 +" libras";
+    var str6 = "cantidad: "+ 2 + 5;
+
+
+//operadores de comparación
+var comparar =  2 == 2;// valores iguales
+    comparar = "2"== 2;// true
+    comparar =  2 === 2; // valores iguales y tipos iguales
+    comparar = "2"=== 2; //false
+    comparar =  2 != 3;  // valores diferentes
+    comparar = "2"!==2; // valores diferentes o tipos diferentes
+    comparar =  3>2;//  true
+    comparar =  2<3;// true
+    comparar = 3>=3;// true
+    comparar = 2<=2;// true
+
+})();
