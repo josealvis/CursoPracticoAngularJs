6eb6f722bb20f8b88f1f49435a968f835db5105b
Jose Alvis <josebb1491@gmail.com>
2017-08-05 17:57:47 -0400

    Update

--- Client/app/controllers/main-controller.js
+++ Client/app/controllers/main-controller.js
@@ -5,11 +5,26 @@ angular
      .module("MyApp")
      .controller('mainController',controller);
 
-     function controller($scope){
-      // $scope.mensaje = "hola mundo.";
-       var vm = this;
-       vm.texto = "desde el Desde el padre";
-       vm.label = "Esto es un gran mensaje!!";
+     function controller(){
+      var vm = this;
+      vm.texto = "";
+      vm.alertarTexto = alertarTexto;
+      vm.listadoComida = ['Pan','Yogurt', 'Queso', 'Leche', 'Arroz'];
+      vm.listadoComida2 = [
+        {producto: 'Pan', precio: 10, fechaEx: new Date()},
+        {producto: 'Yogurt', precio: 40, fechaEx: new Date()},
+        {producto: 'Queso', precio: 100, fechaEx: new Date()},
+        {producto: 'Leche', precio: 60, fechaEx: new Date()},
+        {producto: 'Arroz', precio: 25, fechaEx: new Date()}
+      ];
+
+
+
+    
+      function alertarTexto(){
+      alert(vm.texto);
+      }
+
 
      }
 
